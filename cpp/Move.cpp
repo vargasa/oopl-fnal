84b8f714410003fdde62b6ebc30eaf2f130a4b38
Glenn Downing <downing@cs.utexas.edu>
2020-08-13 21:37:43 -0500

    another commit

--- cpp/Move.cpp
+++ cpp/Move.cpp
@@ -2,160 +2,145 @@
 // Move.c++
 // --------
 
+// https://en.cppreference.com/w/cpp/utility/move
+
 #include <cassert>  // assert
 #include <iostream> // cout, endl
 #include <utility>  // move
 
 using namespace std;
 
-template <typename T>
 struct A {
-    A () {
-        cout << "A() ";}
+    A () = default;
 
     A (const A&) {               // copy constructor
         cout << "A(const A&) ";}
 
-    A (A&&) {                    // move constructor
-        cout << "A(A&&) ";}
-
     A& operator = (const A&) {   // copy assignment
         cout << "=(const A&) ";
-        return *this;}
-
-    A& operator = (A&&) {        // move assignment
-        cout << "=(A&&) ";
-        return *this;}
-
-    ~A() {
-        cout << "~A() ";}};
+        return *this;}};
 
 
 
-template <typename T>
+A f () {
+    A x;
+    return x;}
+
+
+
+void test1 () {
+    cout << "1. A copy constructor: ";
+    A x;
+    A y = x;                           // l-value, A(const A&)
+    assert(&y);}
+
+void test2 () {
+    cout << "2. A copy elision ";
+    A y = f();                         // r-value
+    assert(&y);}
+
+void test3 () {
+    cout << "3. A copy assignment: ";
+    A x;
+    A y;
+    y = x;}                            // l-value, =(const A&)
+
+void test4 () {
+    cout << "4. A copy assignment: ";
+    A y;
+    y = f();}                          // r-value, =(const A&)
+
+
+
 struct B {
-    A<T> _x;                       // containment
+    B () = default;
 
-    B () :
-            _x () {
-        cout << "B() ";}
-
-    B (const B& rhs) :
-            _x (rhs._x) {
+    B (const B&) {               // copy constructor
         cout << "B(const B&) ";}
 
-    B& operator = (const B& rhs) {
-        _x = rhs._x;
+    B (B&&) {                    // move constructor
+        cout << "B(B&&) ";}
+
+    B& operator = (const B&) {   // copy assignment
         cout << "=(const B&) ";
         return *this;}
 
-    ~B() {
-        cout << "~B() ";}};
-
-void test1 () {
-    cout << "B. copy cntr: ";
-    B<int> x;                 // A() B()
-    B<int> y = x;             // A(const A&) B(const B&);
-    }
-
-void test2 () {
-    cout << "B. move cntr: ";
-    B<int> x;                 // A() B()
-    B<int> y = move(x);       // A(const A&) B(const B&);
-    }
-
-void test3 () {
-    cout << "B. copy asgn: ";
-    B<int> x;                 // A() B()
-    B<int> y;                 // A() B()
-    y = x;                    // =(const A&) =(const B&)
-    }
-
-void test4 () {
-    cout << "B. move asgn: ";
-    B<int> x;                 // A() B()
-    B<int> y;                 // A() B()
-    y = move(x);              // =(const A&) =(const B&)
-    }
+    B& operator = (B&&) {        // move assignment
+        cout << "=(B&&) ";
+        return *this;}};
 
 
 
-template <typename T>
-struct C {
-    A<T> _x;                       // containment
+B g () {
+    B x;
+    return x;}
 
-    C () :
-            _x () {
-        cout << "C() ";}
 
-    C (const C& rhs) :
-            _x (rhs._x) {
-        cout << "C(const C&) ";}
-
-    C (C&& rhs) :
-            _x (move(rhs._x)) {
-        cout << "C(C&&) ";}
-
-    C& operator = (const C& rhs) {
-        _x = rhs._x;
-        cout << "=(const C&) ";
-        return *this;}
-
-    C& operator = (C&& rhs) {
-        _x = move(rhs._x);
-        cout << "=(C&&) ";
-        return *this;}
-
-    ~C() {
-        cout << "~C() ";}};
 
 void test5 () {
-    cout << "C. copy cntr: ";
-    C<int> x;                 // A() C()
-    C<int> y = x;             // A(const A&) C(const C&)
-    }
+    cout << "5. B move constructor: ";
+    B x;
+    B y = move(x);                     // l-value, B(const B&)
+    assert(&y);}
 
 void test6 () {
-    cout << "C. move cntr: ";
-    C<int> x;                 // A() C()
-    C<int> y = move(x);       // A(A&&) C(C&&)
-    }
+    cout << "6. B move elision ";
+    B y = g();                         // r-value
+    assert(&y);}
 
 void test7 () {
-    cout << "C. copy asgn: ";
-    C<int> x;                 // A() C()
-    C<int> y;                 // A() C()
-    y = x;                    // =(const A&) =(const C&)
-    }
+    cout << "7. B move assignment: ";
+    B x;
+    B y;
+    y = move(x);}                      // l-value, =(const B&)
 
 void test8 () {
-    cout << "C. move asgn: ";
-    C<int> x;                 // A() C()
-    C<int> y;                 // A() C()
-    y = move(x);              // =(A&&) =(C&&)
-    }
+    cout << "8. B move assignment: ";
+    B y;
+    y = g();}                          // r-value, =(B&&)
+
+
 
 int main () {
     cout << "Move.c++" << endl;
-    test1(); cout << endl;
-    test2(); cout << endl;
-    test3(); cout << endl;
-    test4(); cout << endl << endl;
-    test5(); cout << endl;
-    test6(); cout << endl;
-    test7(); cout << endl;
-    test8(); cout << endl;
+    test1();
+    cout << endl;
+
+    test2();
+    cout << endl << endl;
+
+    test3();
+    cout << endl;
+
+    test4();
+    cout << endl << endl;
+
+    test5();
+    cout << endl;
+
+    test6();
+    cout << endl << endl;
+
+    test7();
+    cout << endl;
+
+    test8();
+    cout << endl;
     cout << "Done." << endl;
     return 0;}
 
 /*
-B. copy cntr: A() B() A(const A&) B(const B&) ~B() ~A() ~B() ~A()
-B. move cntr: A() B() A(const A&) B(const B&) ~B() ~A() ~B() ~A()
-B. copy asgn: A() B() A() B() =(const A&) =(const B&) ~B() ~A() ~B() ~A()
-B. move asgn: A() B() A() B() =(const A&) =(const B&) ~B() ~A() ~B() ~A()
+Move.c++
+1. A copy constructor: A(const A&)
+2. A copy elision
 
-C. copy cntr: A() C() A(const A&) C(const C&) ~C() ~A() ~C() ~A()
-C. move cntr: A() C() A(A&&) C(C&&) ~C() ~A() ~C() ~A()
-C. copy asgn: A() C() A() C() =(const A&) =(const C&) ~C() ~A() ~C() ~A()
-C. move asgn: A() C() A() C() =(A&&) =(C&&) ~C() ~A() ~C() ~A()
+3. A copy assignment: =(const A&)
+4. A copy assignment: =(const A&)
+
+5. B move constructor: B(B&&)
+6. B move elision
+
+7. B move assignment: =(B&&)
+8. B move assignment: =(B&&)
+Done.
 */
--- cpp/UniquePtr.cpp
+++ cpp/UniquePtr.cpp
@@ -4,8 +4,10 @@
 
 // http://en.cppreference.com/w/cpp/memory/unique_ptr
 
+#include <algorithm> // swap
 #include <cassert>   // assert
 #include <iostream>  // cout, endl
+#include <utility>    // move
 
 using namespace std;
 
@@ -20,8 +22,19 @@ class my_unique_ptr {
         explicit my_unique_ptr (T* p) {
             _p = p;}
 
-        my_unique_ptr             (const my_unique_ptr&) = delete;
+        my_unique_ptr (const my_unique_ptr&) = delete;
+
+        my_unique_ptr (my_unique_ptr&& rhs) {
+            swap(_p, rhs._p);}
+
         my_unique_ptr& operator = (const my_unique_ptr&) = delete;
+
+        my_unique_ptr& operator = (my_unique_ptr&& rhs) {
+            if (this == &rhs)
+                return *this;
+            my_unique_ptr that(move(rhs));
+            swap(_p, that._p);
+            return *this;}
 
         ~my_unique_ptr () {
             delete _p;}
@@ -29,18 +42,65 @@ class my_unique_ptr {
         T* get () const {
             return _p;}};
 
-void test1 () {
-    my_unique_ptr<A> x;          // default constructor
+struct A {
+    static int c; // number of current A instances
+
+    A () {
+        ++c;}
+
+    ~A () {
+        --c;}};
+
+int A::c = 0;
+
+void test0 () {
+    assert(A::c == 0);
+    my_unique_ptr<A> x;         // default constructor
+    assert(A::c    == 0);
     assert(x.get() == nullptr);}
 
-void test2 () {
+void test1 () {
+    assert(A::c == 0);
+    {
 //  my_unique_ptr<A> x = new A; // error: conversion from 'A*' to non-scalar type 'my_unique_ptr<A>' requested
     my_unique_ptr<A> x(new A);  // pointer constructor
+    assert(A::c    == 1);
     assert(x.get() != nullptr);
+    }
+    assert(A::c == 0);}
+
+void test2 () {
+    assert(A::c == 0);
+    {
+    my_unique_ptr<A> x(new A);
+    assert(A::c    == 1);
+//  my_unique_ptr<A> y = x;       // error: use of deleted function 'my_unique_ptr<T>::my_unique_ptr(const my_unique_ptr<T>&) [with T = A]'
+    my_unique_ptr<A> y = move(x); // move constructor
+    assert(A::c    == 1);
+    assert(x.get() == nullptr);
+    assert(y.get() != nullptr);
+    }
+    assert(A::c == 0);}
+
+void test3 () {
+    assert(A::c == 0);
+    {
+    my_unique_ptr<A> x(new A);
+    my_unique_ptr<A> y(new A);
+    assert(A::c == 2);
+//  y = x;                      // error: use of deleted function 'my_unique_ptr<T>& my_unique_ptr<T>::operator=(const my_unique_ptr<T>&) [with T = A]'
+    y = move(x);                // move assignment operator
+    assert(x.get() == nullptr);
+    assert(y.get() != nullptr);
+    assert(A::c    == 1);
+    }
+    assert(A::c == 0);}
 
 int main () {
     cout << "UniquePtr.c++" << endl;
+    test0();
     test1();
     test2();
+    test3();
     cout << "Done." << endl;
     return 0;}
